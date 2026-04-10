d618a559a464dab8bd6ac65e59621e0537287083
Vaibhav Pant <functionalrustic09@gmail.com>
2026-04-11 00:51:56 +0530

    Initial commit - AI Code Reviewer project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Brain , Sun } from 'lucide-react';
+import Editor from '@monaco-editor/react';  
+
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="nav flex items-center justify-between h-[90px] bg-zinc-900" style={{padding:"0px 150px"}}>
+        <div className="logo flex items-center gap-[10px]">
+            <Brain size ={30} color='#9333ea'/>
+            <span className="text-2xl font-bold text-white ml-2 cursor-pointer transition-all hover:text-[#9333ea] ">CodeNinja</span>
+        </div>
+        <div className="icons flex items-center gap-[20px]">
+            <i className='cursor-pointer transition-all hover:text-[#9333ea]'><Sun/></i>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
